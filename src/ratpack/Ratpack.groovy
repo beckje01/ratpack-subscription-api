cb08f80b2a85ea07a8efbc407bfd96652c97b925
Jeff Beck <jeff.beck@reachlocal.com>
2014-07-26 09:16:38 -0500

    Stub of subscription resource support.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,12 +1,41 @@
-import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-  handlers {
-    get {
-      render groovyTemplate("index.html", title: "My Ratpack App")
+    handlers {
+        handler("subscriptions") {
+            byMethod {
+                get {
+                    def cid = request.queryParams['cid']
+                    if (cid) {
+                        render "Got CID ${cid}"
+                    } else {
+                        response.status 422
+                        response.send("application/json", /{"error": "Must set cid"}/)
+                    }
+                }
+                post {
+                    //TODO create a subscription
+                    render "POST new subscription"
+                }
+            }
+        }
+
+        handler("subscriptions/:id") {
+            def id = pathTokens['id']
+            if (!id) {
+                response.status 422
+                response.send()
+            }
+            byMethod {
+                get {
+                    //TODO display subscription
+                    render "GET $id"
+                }
+                put {
+                    //TODO update subscription
+                    render "PUT $id"
+                }
+            }
+        }
     }
-        
-    assets "public"
-  }
 }
